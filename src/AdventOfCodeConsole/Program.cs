e39965843ec20336ae23efcff761dc94bb60b1c5
Jordy van Eijk <jcvaneijk@gmail.com>
2024-12-02 11:55:11 +0100

    Finished day 1

---
+++ src/AdventOfCodeConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using AocDay1;
+using Common;
+using Spectre.Console;
+
+namespace AdventOfCodeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = new Dictionary<int, IDay?>
+            {
+                { 1, new Day1() },
+                // Add other days here
+            };
+
+            while (true)
+            {
+                var day = AnsiConsole.Prompt(
+                    new SelectionPrompt<int>()
+                        .Title("Choose a day in December [green](1-31)[/], or [red]0 to exit[/]:")
+                        .PageSize(15)
+                        .MoreChoicesText("[grey](Move up and down to reveal more days)[/]")
+                        .AddChoices(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31));
+
+                if (day == 0)
+                {
+                    break;
+                }
+
+                if (days.TryGetValue(day, out IDay? selectedDay))
+                {
+                    selectedDay!.Run();
+                }
+                else
+                {
+                    AnsiConsole.MarkupLine($"[red]Day {day} is not implemented yet.[/]");
+                }
+            }
+        }
+    }
+}
---
+++ src/AocDay1/Day1.cs
@@ -0,0 +1,81 @@
+﻿using Common;
+
+namespace AocDay1;
+
+public class Day1 : IDay
+{
+    public void Run()
+    {
+        Process();
+        Console.WriteLine("Day 1");
+    }
+
+    private void Process()
+    {
+        var lines = File.ReadAllLines("Input/input.txt");
+        var leftNumbers = new List<int>();
+        var rightNumbers = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            int left = int.Parse(parts[0]);
+            int right = int.Parse(parts[1]);
+
+            leftNumbers.Add(left);
+            rightNumbers.Add(right);
+        }
+
+        leftNumbers = leftNumbers.Order().ToList();
+        rightNumbers = rightNumbers.Order().ToList();
+        var totalDistance = FindDistance(leftNumbers, rightNumbers);
+        var totalSimilarity = FindSimilarity(leftNumbers, rightNumbers);
+
+        Console.WriteLine($"Total distance: {totalDistance}");
+        Console.WriteLine($"Total similarity: {totalSimilarity}");
+    }
+
+    private static int FindDistance(List<int> leftNumbers, List<int> rightNumbers)
+    {
+        int totalDistance = 0;
+        for (int i = 0; i < leftNumbers.Count; i++)
+        {
+            int left = leftNumbers[i];
+            int right = rightNumbers[i];
+            int distance;
+
+            if (left > right)
+            {
+                distance = left - right;
+            }
+            else
+            {
+                distance = right - left;
+            }
+
+            Console.WriteLine($"{i} --> {distance}");
+
+            totalDistance += distance;
+        }
+
+        return totalDistance;
+    }
+
+    private static long FindSimilarity(List<int> leftNumbers, List<int> rightNumbers)
+    {
+        long totalSimilarity = 0L;
+        for (int i = 0; i < leftNumbers.Count; i++)
+        {
+            int left = leftNumbers[i];
+            int countedRight = rightNumbers.Count(x => x == left);
+            int similarity = left * countedRight;
+
+
+            Console.WriteLine($"{i} --> {countedRight} * {left} = {similarity}");
+
+            totalSimilarity += similarity;
+        }
+
+        return totalSimilarity;
+    }
+}
